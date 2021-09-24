80adfedc46c5dea715742740c1bdcbaef1743e5f
Alexandr Rudy <aoikomete@gmail.com>
2021-09-24 12:01:24 +0300

    SYS: init plugin

---
+++ jet-engine-single-products-widget-custom-visibility-conditions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: JetEngine - Single Product Widgets Custom Visibility Conditions
+ * Plugin URI: https://github.com/UraraReika/jet-engine-single-products-widget-custom-visibility-conditions
+ * Description:
+ * Version:     1.0.0
+ * Author:      Crocoblock
+ * Author URI:  https://crocoblock.com/
+ * Text Domain: jet-engine
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die();
+}
